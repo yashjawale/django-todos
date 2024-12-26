19d1d93f82d2474095719c0936f1615323ae0556
yashjawale <yash25.j@gmail.com>
2024-12-26 20:42:51 +0530

    Complete tasks page

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Task
 
 def index(request):
     return render(request, 'home.html')
@@ -11,7 +12,10 @@ def register(request):
     return render(request, 'register.html')
 
 def taskdetails(request, task_id):
-    return render(request, 'taskdetails.html', {'task_id': task_id})
+    task = Task.objects.filter(task_id=task_id)
+    return render(request, 'taskdetails.html', {'task': task})
 
 def tasks(request):
-    return render(request, 'tasks.html')
+    pending_tasks = Task.objects.filter(completed=False).order_by('-deadline')
+    completed_tasks = Task.objects.filter(completed=False).order_by('-id')
+    return render(request, 'tasks.html', {'pending_tasks': pending_tasks, 'completed_tasks': completed_tasks})
