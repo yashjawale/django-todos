37b92622b75ef5882abd5994c2f41ab33dc66203
yashjawale <yash25.j@gmail.com>
2024-12-26 23:11:47 +0530

    Add new task modal

--- tasks/views.py
+++ tasks/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Task
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from . import forms
 
 def index(request):
     return render(request, 'home.html')
@@ -21,4 +22,5 @@ def taskdetails(request, task_id):
 def tasks(request):
     pending_tasks = Task.objects.filter(completed=False).order_by('-deadline')
     completed_tasks = Task.objects.filter(completed=False).order_by('-id')
-    return render(request, 'tasks.html', {'pending_tasks': pending_tasks, 'completed_tasks': completed_tasks})
+    form = forms.CreateTask()
+    return render(request, 'tasks.html', {'pending_tasks': pending_tasks, 'completed_tasks': completed_tasks, 'form': form})
