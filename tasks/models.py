89cc91037aec4fd4c6b9a6d11a4d7b5351f5f448
yashjawale <yash25.j@gmail.com>
2024-12-26 13:57:49 +0530

    Add task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Task (models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    deadline = models.DateTimeField()
