9062fe432ffd0d8f344a0f611c399c24854b7393
yashjawale <yash25.j@gmail.com>
2024-12-30 18:35:13 +0530

    Minor changes to settings.py

--- djangotodos/settings.py
+++ djangotodos/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ex+6b=3uxx(+k#7*373@71&#hgyf^5c&w=9$k-!eb%0)=p4md_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 # Application definition
 
